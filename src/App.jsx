cc59224c36d35b6503ff237172cb16f10531f769
David Sarria <david.sarriav@gmail.com>
2023-05-01 20:29:20 +0200

    Versión funcional de la lista, sin estilos, listos estados para conectar modulo formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import Header from './components/Header/Header'
 import ProductList from './components/ProductList/ProductList'
-
+import { products as productList } from "./data"
 import { useState } from 'react'
 import './App.css'
 
 
 function App() {
 
+  const [products, setProducts] = useState(productList)
+
+  const addProduct = (newProduct) => {
+    const newID = new Date();
+    newProduct.id = newID;
+    setProducts([...products, newProduct])
+  }
+
   return (
     <>
       <Header />
       <div className='main-container'>
-        <ProductList />
+        <ProductList products={products}/>
       </div>
     </>
   )
